b9b663f7bd8d2964b3f4d8709d970cdb9bccd7e0
matthew <teckousa@gmail.com>
2020-10-28 16:45:41 -0400

    Assignment 3 branch

--- src/MyPoint.java
+++ src/MyPoint.java
@@ -10,20 +10,4 @@ public class MyPoint {
     public String toString(){
         return "("+this.x+", "+this.y+")";
     }
-
-    @Override
-    public int hashCode(){
-        int hashcode = 0;
-        hashcode = (int) x*20;
-        hashcode += toString().hashCode();
-        return hashcode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (!(o instanceof MyPoint)) return false;
-        MyPoint c = (MyPoint) o;
-        return Double.compare(x, c.x) == 0 && Double.compare(y, c.y) == 0;
-    }
 }
